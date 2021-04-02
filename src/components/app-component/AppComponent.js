d021088667b89f95813d45cbe40b34a6a751ca09
Matthew Pagan <mastapegs01@gmail.com>
2021-04-02 11:13:51 -0400

    added header

--- src/components/app-component/AppComponent.js
+++ src/components/app-component/AppComponent.js
@@ -1,7 +1,23 @@
-import { LitElement, html } from 'lit-element'
+import { LitElement, html, css } from 'lit-element'
 import '../noah-renderer'
 
 export default class AppComponent extends LitElement {
+  static get styles() {
+    return [
+      css`
+        h1 {
+          text-align: center;
+          background-color: hsla(0, 0%, 30%, 1);
+          color: white;
+          text-shadow: 0 5px 10px black;
+          margin: 0;
+          padding: 1rem 0;
+          border-bottom: 5px solid hsla(240, 50%, 20%, 1)
+        }
+      `
+    ]
+  }
+
   render() {
     return html`
       <h1>So Many Noahs!!!!!</h1>
