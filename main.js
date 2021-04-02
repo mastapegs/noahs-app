707925ecc73366358c39691a8a695b04fd4a2280
Matthew Pagan <mastapegs01@gmail.com>
2021-04-02 10:49:19 -0400

    Convert to LitElement (#1)

--- main.js
+++ main.js
@@ -1,29 +1,54 @@
-import './style.css'
+import { LitElement, html, css } from 'lit-element'
+import { styleMap } from 'lit-html/directives/style-map'
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
-
-const randomColor = () => {
-  return `rgb(${getRandomInt(256)}, ${getRandomInt(256)}, ${getRandomInt(256)})`
-}
-
-const renderNoahs = () => {
-  const list = []
-  for (let i = 0; i < 1_500; i++) {
-    list.push(`<div
-      style="
-        background: ${randomColor()};
-        color: ${randomColor()};
-      "
-      class="grid-item"
-    >Noah</div>`)
+export default class AppComponent extends LitElement {
+  static get styles() {
+    return [
+      css`
+        .grid {
+          display: grid;
+          grid-template-columns: repeat(auto-fill, minmax(40px, 1fr));
+          place-items: center stretch;
+          text-align: center;
+        }
+        .grid > * {
+          border: 1px solid black;
+          text-shadow: 1px 1px black;
+        }
+      `,
+    ]
   }
-  return list.join('')
+
+  __getRandomInt(max) {
+    return Math.floor(Math.random() * max)
+  }
+  
+  __getRandomColor() {
+    return `rgb(${this.__getRandomInt(256)}, ${this.__getRandomInt(256)}, ${this.__getRandomInt(256)})`
+  }
+  
+  __getNoahStyles() {
+    return styleMap({
+      background: this.__getRandomColor(),
+      color: this.__getRandomColor(),
+    })
+  }
+  
+  __renderNoahs() {
+    const listOfNoahs = []
+    for (let i = 0; i < 1_500; i++) {
+      listOfNoahs.push(html`<div style=${this.__getNoahStyles()}>Noah</div>`)
+    }
+    return listOfNoahs
+  }
+
+  render() {
+    return html`
+      <div class="grid">
+        ${this.__renderNoahs()}
+      </div>
+    `
+  }
 }
 
-document.querySelector('#app').innerHTML = `
-  <div class="grid">
-    ${renderNoahs()}
-  </div>
-`
+customElements.define('app-component', AppComponent)
