5c46491f02c903f46bde8d486d44164bdc7a0f14
Matthew Pagan <mastapegs01@gmail.com>
2021-04-02 09:24:09 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+import './style.css'
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+const randomColor = () => {
+  return `rgb(${getRandomInt(256)}, ${getRandomInt(256)}, ${getRandomInt(256)})`
+}
+
+const renderNoahs = () => {
+  const list = []
+  for (let i = 0; i < 5_000; i++) {
+    list.push(`<div
+      style="
+        background: ${randomColor()};
+        color: ${randomColor()};
+      "
+      class="grid-item"
+    >Noah</div>`)
+  }
+  return list.join('')
+}
+
+document.querySelector('#app').innerHTML = `
+  <div class="grid">
+    ${renderNoahs()}
+  </div>
+`
