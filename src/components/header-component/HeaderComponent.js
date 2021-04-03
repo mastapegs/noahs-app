19482f8f55582ba45d37bda27caae3ceaaa8132a
Matthew Pagan <mastapegs01@gmail.com>
2021-04-03 10:19:11 -0400

    better alignment

--- src/components/header-component/HeaderComponent.js
+++ src/components/header-component/HeaderComponent.js
@@ -8,19 +8,16 @@ export default class HeaderComponent extends LitElement {
         header {
           display: flex;
           justify-content: space-evenly;
-          align-content: center;
           border-bottom: 5px solid hsla(240, 50%, 20%, 1);
           background-color: hsla(0, 0%, 30%, 1);
           color: white;
+          height: 100px;
         }
         h1 {
           text-align: center;
           text-shadow: 0 5px 10px black;
-          margin: 0;
+          margin: auto 0;
           padding: 1rem 0;
-        }
-        img {
-          height: 100%;
         }
       `
     ]
@@ -29,15 +26,11 @@ export default class HeaderComponent extends LitElement {
   render() {
     return html`
       <header>
-        <div>
-          <img src=${rainbowDragon} alt="Rainbow Dragon">
-        </div>
+        <img src=${rainbowDragon} alt="Rainbow Dragon">
         <h1>
           <slot name="text"></slot>
         </h1>
-        <div>
-          <img src=${rainbowDragon} alt="Rainbow Dragon">
-        </div>
+        <img src=${rainbowDragon} alt="Rainbow Dragon">
       </header>
     `
   }
