3cd8f024693e86c92e501f06588b08fbffeba842
Matthew Pagan <mastapegs01@gmail.com>
2021-04-03 14:57:35 -0400

    silly doodle

--- src/components/test-svg/TestSvg.js
+++ src/components/test-svg/TestSvg.js
@@ -1,9 +1,42 @@
-import { LitElement, html } from 'lit-element'
+import { LitElement, html, css } from 'lit-element'
 
 export default class AppComponent extends LitElement {
+  static get styles() {
+    return [
+      css`
+        .container {
+          display: flex;
+          justify-content: center;
+          padding: 1rem;
+          background-color: hsl(180, 50%, 80%);
+        }
+        svg {
+          stroke: black;
+          stroke-width: 5px;
+        }
+      `,
+    ]
+  }
   render() {
     return html`
-      <p>Test SVG</p>
+      <div class="container">
+        <svg width="100" height="100">
+          <line x1="0" y1="0" x2="100" y2="0" />
+          <line x1="100" y1="0" x2="100" y2="100" />
+          <line x1="100" y1="100" x2="0" y2="100" />
+          <line x1="0" y1="100" x2="0" y2="0" />
+        
+          <rect x="10" y="10" width="30" height="30" fill="red"/>
+          <circle cx="80" cy="50" r="15" fill="blue" />
+          <path d="
+            M 10 90
+            C 20 80 -5 40 10 50
+            S 20 120 30 80
+            S 0 50 40 50
+            S 60 100 90 90
+          " fill="transparent" stroke-width="3" />
+        </svg>
+      </div>
     `
   }
 }
