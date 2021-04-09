c06b7e74b9b1c0c7835aa78246ad48a26a1cc74a
Matthew Pagan <mastapegs01@gmail.com>
2021-04-09 19:43:13 -0400

    fix spinning widget

--- src/components/silly-doodle-svg/SillyDoodleSvg.js
+++ src/components/silly-doodle-svg/SillyDoodleSvg.js
@@ -1,36 +1,52 @@
 import { LitElement, html } from 'lit-element'
+import { classMap } from 'lit-html/directives/class-map'
 import svgStyles from './css.js'
 
 export default class SillyDoodleSvg extends LitElement {
+  static get properties() {
+    return {
+      spin: { type: Boolean }
+    }
+  }
+
   static get styles() {
     return [
       svgStyles,
     ]
   }
 
+  constructor() {
+    super()
+    this.spin = false
+  }
+
   __handleSvgClick(e) {
-    console.log('spin!!!')
-    e.target.classList.add('spin')
+    this.spin = true
+    console.log(this.spin)
     e.target.addEventListener("animationend", () => {
-      console.log('spin over')
-      this.classList.remove('spin')
+      this.spin = false
+      console.log(this.spin)
     })
   }
-  
+
+  __getSvgClass() {
+    return classMap({ spin: this.spin })
+  }
+
   render() {
     return html`
       <div class="container">
         <p>A super silly doodle</p>
-        <svg @click=${this.__handleSvgClick} width="100" height="100">
-          <rect x="10" y="10" width="30" height="30" fill="red"/>
+        <svg class=${this.__getSvgClass()} @click=${this.__handleSvgClick} width="100" height="100">
+          <rect x="10" y="10" width="30" height="30" fill="red" />
           <circle cx="65" cy="30" r="15" fill="blue" />
           <path d="
-            M 10 90
-            C 20 80 -5 40 10 50
-            S 20 120 30 80
-            S 0 50 40 50
-            S 60 100 90 90
-          " fill="transparent" stroke-width="3" />
+                        M 10 90
+                        C 20 80 -5 40 10 50
+                        S 20 120 30 80
+                        S 0 50 40 50
+                        S 60 100 90 90
+                      " fill="transparent" stroke-width="3" />
         </svg>
       </div>
     `
